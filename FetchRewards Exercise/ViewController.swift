af533d8ad3f81d2d37457bdac2361c26ab4e37f6
dambarbista1990 <dambarbista.db@gmail.com>
2020-11-21 13:48:55 -0500

    Initial Commit

---
+++ FetchRewards Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchRewards Exercise
+//
+//  Created by Dambar Bista on 11/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
